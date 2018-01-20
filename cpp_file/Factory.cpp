44c8f4ab8b2b83f68fc497d883960c7934c60eed
Wzorek <wzorek.piotr@gmail.com>
2018-01-20 20:06:40 +0100

    Coś zaczołem pisać xD Ale wracam do naprawiania nagłówków najpierw

---
+++ cpp_file/Factory.cpp
@@ -0,0 +1,38 @@
+#include "../header/Factory.hpp"
+
+// Ramps
+void Factory::addRamp(Ramp _ramp){
+
+}
+
+std::list<Ramp> Factory::getRamps(){
+}
+
+void Factory::removeRamp(){
+}
+
+// Workers
+
+void Factory::addWorker(){
+}
+
+std::list<workers> Factory::getWorkers(){
+}
+
+void Factory::removeWorker(){
+
+}
+
+// Storehouses
+
+void Factory::addStorehouse(){
+}
+
+std::list<storehouses> Factory::getStorehouses(){
+}
+
+void Factory::removeStorehouse(){
+
+}
+
+bool Factory::isConsistent();
